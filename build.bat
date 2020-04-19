d0b70e539120e2d3a6620695d299d0bf047da44a
Jens Munk Hansen <jem@bkmedical.com>
2020-04-19 16:38:36 +0200

    updated GUI

--- build.bat
+++ build.bat
@@ -20,4 +20,10 @@ cd %~dp0\build
 REM Build solution
 msbuild TrialVTK.sln /p:Configuration=Release
 
+set CURRENTDRIVE=%CD:~0,2%
+%QTDIR%\bin\qtenv2.bat
+cd /D %CURRENTDRIVE%%~dp0\build\Registration\Registration\Release
+%QTDIR%\bin\windeployqt.exe Registration.exe
+
+
 cd %~dp0
