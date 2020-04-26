a8d43e04b47d45f9365c2d1d4ddcdfb6fce5a383
Jens Munk Hansen <jem@bkmedical.com>
2020-04-26 20:59:32 +0200

    ..

---
+++ Registration/python/reslice.py
@@ -0,0 +1,121 @@
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+import vtk
+import numpy as np
+import math
+
+import matplotlib.pyplot as plt
+
+plt.ion()
+
+autoCrop = True
+dimReduce = True
+#autoCrop = False
+#dimReduce = False
+# Without autocrop and dimensionality, you can set output origin is always zero
+
+def vtkToNumpy(data):
+    temp = vtk_to_numpy(data.GetPointData().GetScalars())
+    dims = data.GetDimensions()
+    numpy_data = temp.reshape(dims[2], dims[1], dims[0])
+    numpy_data = numpy_data.transpose(2,1,0)
+    return numpy_data
+
+def numpyToVTK(data):
+    flat_data_array = data.transpose(2,1,0).flatten()
+    vtk_data_array = numpy_to_vtk(flat_data_array)
+    vtk_data = numpy_to_vtk(num_array=vtk_data_array, deep=True, array_type=vtk.VTK_FLOAT)
+    img = vtk.vtkImageData()
+    img.GetPointData().SetScalars(vtk_data)
+    img.SetDimensions(data.shape)
+    return img
+
+img = np.zeros(shape=[512,512,120])
+img[0:300,0:100,:] = 1
+
+vtkImg = numpyToVTK(img)
+
+reslice = vtk.vtkImageReslice()
+reslice.SetInputData(vtkImg)
+if autoCrop:
+  reslice.SetAutoCropOutput(True)
+else:
+  reslice.SetAutoCropOutput(False)
+
+if dimReduce:
+  reslice.SetOutputDimensionality(2)
+
+reslice.SetInterpolationModeToCubic()
+reslice.SetSlabNumberOfSlices(1)
+reslice.SetOutputSpacing(1.0,1.0,1.0)
+
+if 1:
+  a = math.pi/4
+  center = [100, 254.5, 40]
+else:
+  a = 0.0
+  center = [256, 1000, 100]
+
+x = [math.cos(a),math.sin(a),0]
+y = [-math.sin(a),math.cos(a),0]
+z = [0,0,1]
+
+
+axialElement = [
+    x[0], y[0], z[0], center[0],
+    x[1], y[1], z[1], center[1],
+    x[2], y[2], z[2], center[2],
+    0,    0,    0,    1
+]
+resliceAxes = vtk.vtkMatrix4x4()
+resliceAxes.DeepCopy(axialElement)
+reslice.SetResliceAxes(resliceAxes)
+reslice.Update()
+
+reslicedImg = reslice.GetOutput()
+
+# SetAutoCropOutput(True) cause vtkImageReslice to choose and origin for the output slice
+
+print("spacing %g %g %g" % reslicedImg.GetSpacing())
+print("origin %g %g %g" % reslicedImg.GetOrigin())
+print("extent %i %i %i %i %i %i" % reslicedImg.GetExtent())
+
+
+# How to obtain coordinate of 3D image for (x,y) in reslicedNpImg?
+
+# Pixel to input voxel
+
+# Multiply output spacing (1,1,1)
+# Add output origin
+# Call vtkImageReslice.GetResliceAxes().MultiplyPoint()
+# Subtract input origin and divide by input spacing
+
+spacing = np.r_[reslicedImg.GetSpacing()]
+origin = np.r_[reslicedImg.GetOrigin()]
+
+pixel = np.r_[0,0]
+
+# Multiply spacing
+tmp = pixel * spacing[0:len(pixel)]
+
+# Add origin
+tmp = np.r_[tmp, 0] + origin
+
+# 3D coordinate of pixel value
+coord = reslice.GetResliceAxes().MultiplyDoublePoint([tmp[0],
+                                                      tmp[1],
+                                                      tmp[2],1])
+print(coord)
+
+reslicedNpImg = vtkToNumpy(reslicedImg)
+
+plt.figure()
+plt.imshow(reslicedNpImg[:,:,0])
+plt.show()
+
+# From 3D coordinates to pixel coordinates
+
+invertAxes = vtk.vtkMatrix4x4()
+vtk.vtkMatrix4x4.Invert(resliceAxes, invertAxes)
+location = invertAxes.MultiplyDoublePoint([coord[0], coord[1], coord[2], 1])
+print(location)
+print([location[0]-origin[0],location[1]-origin[1],location[2]-origin[2]])
--- Registration/python/vesselSeg.py
+++ Registration/python/vesselSeg.py
@@ -1,12 +1,27 @@
 import sys
+import os
 import numpy as np
+
+from matplotlib import pyplot as plt
+
+plt.ion()
+plt.close('all')
 
 import SimpleITK as sitk
 
-sys.path.append('/home/jmh/github/ITK/SimpleITK-Notebooks/Utilities')
+from timeit import default_timer as timer
+
+if os.name == 'nt':
+  sys.path.append('e:/github/SimpleITK-Notebooks/Utilities')
+else:
+  sys.path.append('/home/jmh/github/ITK/SimpleITK-Notebooks/Utilities')
+
 from myshow import myshow, myshow3d
 
-img = sitk.ReadImage('/home/jmh/bkmedical/data/CT/CT-Abdomen.mhd')
+if os.name == 'nt':
+  img = sitk.ReadImage('e:/datavtk/CT/CT-Abdomen.mhd')
+else:
+  img = sitk.ReadImage('/home/jmh/bkmedical/data/CT/CT-Abdomen.mhd')
 
 # itk::IntensityWindowingImageFilter
 
@@ -18,38 +33,18 @@ window = 50
 
 imax = level + 0.5*window
 imin = level - 0.5*window
-print(imax)
-print(imin)
 
-img255 = sitk.Cast(sitk.RescaleIntensity(img, imin, imax),sitk.sitkUInt8)
+ifilter = sitk.IntensityWindowingImageFilter()
+ifilter.SetWindowMaximum(imax)
+ifilter.SetWindowMinimum(imin)
+img = ifilter.Execute(img)
 
-if 0:
-  filter = sitk.MinimumMaximumImageFilter()
-  filter.Execute(img255)
-  ImageHighestIntensity = filter.GetMaximum()
-  ImageLowestIntensity = filter.GetMinimum()
-#filter.Dispose()
-
-# RescaleIntensityImageFilter (used by function above)
-# range = max - min
-# level = 0.5*(max + min)
-
-f2 = sitk.IntensityWindowingImageFilter()
-
-f2.SetWindowMaximum(170)
-f2.SetWindowMinimum(120)
-f2.Execute(img)
+img255 = sitk.Cast(sitk.RescaleIntensity(img), sitk.sitkUInt8)
 
 size = img.GetSize()
-myshow3d(img255, xslices=range(50, size[0] - 50, 30), title='CT')
+#myshow3d(img255, xslices=range(50, size[0] - 50, 30), title='CT')
 
-#sys.exit(0)
-
-# -164;164   -560:-152
-
-# seed = np.r_[-95.1441, -622.891, -249.269]
-
-seed = (132, 142, 340) # 96
+seed = (185, 217, 407)
 
 seg = sitk.Image(img.GetSize(), sitk.sitkUInt8)
 seg.CopyInformation(img)
@@ -57,14 +52,141 @@ seg[seed] = 1
 
 seg = sitk.BinaryDilate(seg, 3)
 
-myshow3d(sitk.LabelOverlay(img255, seg),
-         xslices=range(132, 133), yslices=range(142, 143),
-         zslices=range(340, 341), title="Initial Seed")
+# sitk.LabelOverlay(img1, sitk.LabelContour(img1_seg), 1.0)
+
+# ImageToVTKImageFilter[itk.Image[itk.UC,3]].New()
+
+if 1:
+  myshow3d(sitk.LabelOverlay(img255, seg),
+           xslices=range(seed[0], seed[0]+1), yslices=range(seed[1], seed[1]+1),
+           zslices=range(seed[2], seed[2]+1), title="Initial Seed")
+
+  # using FilterType = itk::BinaryMask3DMeshSource<ImageType, MeshType>;
+  # FilterType::Pointer filter = FilterType::New();
+  # filter->SetInput(threshold->GetOutput());
+  # filter->SetObjectValue(255);
+  #
+  # using WriterType = itk::MeshFileWriter<MeshType>;
+  # WriterType::Pointer writer = WriterType::New();
+  # writer->SetFileName(outputFileName);
+  # writer->SetInput(filter->GetOutput());
 
 
-seg_con = sitk.ConnectedThreshold(img, seedList=[seed],
-                                  lower=100, upper=190)
+if 1:
+  start = timer()
+  seg_con = sitk.ConnectedThreshold(img, seedList=[seed],
+                                    lower=230, upper=255)
+  end = timer()
+  print('\nSeconds elapsed: %f' % (end - start))
 
-myshow3d(sitk.LabelOverlay(img255, seg_con),
-         xslices=range(132, 133), yslices=range(142, 143),
-         zslices=range(340, 341), title="Connected Threshold")
+  myshow3d(sitk.LabelOverlay(img255, seg_con, colormap = [255,0,0]),
+           xslices=range(seed[0], seed[0]+1), yslices=range(seed[1], seed[1]+1),
+           zslices=range(seed[2], seed[2]+1), title="Connected Threshold")
+
+# TODO: Save and create mesh using VTK
+sitk.WriteImage(seg_con, 'seg.mhd')
+
+
+
+
+
+
+  # TODO: Make surface and show
+
+
+  # Unlike in ``ConnectedThreshold``, you need not select the bounds in
+  # ``ConfidenceConnected`` filter. Bounds are implicitly specified as
+  # :math:`\mu\pm c\sigma`, where :math:`\mu` is the mean intensity of the seed
+  # points, :math:`\sigma` their standard deviation and :math:`c` a user specified
+  # constant.
+  #
+  # This algorithm has some flexibility which you should familiarize yourself with:
+  #
+  # * The ``multiplier`` parameter is the constant :math:`c` from the formula above.
+  # * You can specify a region around each seed point ``initialNeighborhoodRadius``
+  #   from which the statistics are estimated, see what happens when you set it to zero.
+  # * The ``numberOfIterations`` allows you to rerun the algorithm. In the first
+  #   run the bounds are defined by the seed voxels you specified, in the
+  #   following iterations :math:`\mu` and :math:`\sigma` are estimated from
+  #   the segmented points and the region growing is updated accordingly.
+
+if 0:
+  start = timer()
+  seg_conf = sitk.ConfidenceConnected(img255, seedList=[seed],
+                                      numberOfIterations=1,
+                                      multiplier=2.5,
+                                      initialNeighborhoodRadius=1,
+                                      replaceValue=1)
+  end = timer()
+  print('\nConfidence connected: %f' % (end - start))
+
+  if 0:
+    vectorRadius = (1, 1, 1)
+    kernel = sitk.sitkBall
+    start = timer()
+    seg_clean = sitk.BinaryMorphologicalClosing(seg_conf,
+                                                vectorRadius,
+                                                kernel)
+    end = timer()
+    print('\nMorphological Closing: %f' % (end - start))
+  seg_clean = seg_conf
+  myshow3d(sitk.LabelOverlay(img255, seg_clean),
+           xslices=range(seed[0], seed[0]+1), yslices=range(seed[1], seed[1]+1),
+           zslices=range(seed[2], seed[2]+1), title="Confidence Connected Threshold")
+
+
+def CreateFrogActor(fileName, tissue):
+    reader = vtk.vtkMetaImageReader()
+    reader.SetFileName(fileName)
+    reader.Update()
+
+    selectTissue = vtk.vtkImageThreshold()
+    selectTissue.ThresholdBetween(tissue, tissue)
+    selectTissue.SetInValue(255)
+    selectTissue.SetOutValue(0)
+    selectTissue.SetInputConnection(reader.GetOutputPort())
+
+    gaussianRadius = 1
+    gaussianStandardDeviation = 2.0
+    gaussian = vtk.vtkImageGaussianSmooth()
+    gaussian.SetStandardDeviations(gaussianStandardDeviation, gaussianStandardDeviation, gaussianStandardDeviation)
+    gaussian.SetRadiusFactors(gaussianRadius, gaussianRadius, gaussianRadius)
+    gaussian.SetInputConnection(selectTissue.GetOutputPort())
+
+    isoValue = 127.5
+    mcubes = vtk.vtkMarchingCubes()
+    mcubes.SetInputConnection(gaussian.GetOutputPort())
+    mcubes.ComputeScalarsOff()
+    mcubes.ComputeGradientsOff()
+    mcubes.ComputeNormalsOff()
+    mcubes.SetValue(0, isoValue)
+
+    smoothingIterations = 5
+    passBand = 0.001
+    featureAngle = 60.0
+    smoother = vtk.vtkWindowedSincPolyDataFilter()
+    smoother.SetInputConnection(mcubes.GetOutputPort())
+    smoother.SetNumberOfIterations(smoothingIterations)
+    smoother.BoundarySmoothingOff()
+    smoother.FeatureEdgeSmoothingOff()
+    smoother.SetFeatureAngle(featureAngle)
+    smoother.SetPassBand(passBand)
+    smoother.NonManifoldSmoothingOn()
+    smoother.NormalizeCoordinatesOn()
+    smoother.Update()
+
+    normals = vtk.vtkPolyDataNormals()
+    normals.SetInputConnection(smoother.GetOutputPort())
+    normals.SetFeatureAngle(featureAngle)
+
+    stripper = vtk.vtkStripper()
+    stripper.SetInputConnection(normals.GetOutputPort())
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(stripper.GetOutputPort())
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    vtkPolyDataXMLWriter
+    return actor
