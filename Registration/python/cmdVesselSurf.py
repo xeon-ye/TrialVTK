88be062976027a324de4d1cf2070403aa40026e5
Jens Munk Hansen <jem@bkmedical.com>
2020-05-25 18:53:01 +0200

    ..

--- Registration/python/cmdVesselSurf.py
+++ Registration/python/cmdVesselSurf.py
@@ -7,78 +7,89 @@
 # vtkPolyDataConnectivityFilter
 # vtkPolyDataMapper
 
+# TODO: Convert from RAS (DICOM/ITK) to LPS (VTK)
+
 import vtk
+import os
 
-fileName = './seg.mhd'
-reader = vtk.vtkMetaImageReader()
-reader.SetFileName(fileName)
-reader.Update()
+filedir = os.path.dirname(os.path.realpath(__file__))
 
-selectTissue = vtk.vtkImageThreshold()
-selectTissue.ThresholdBetween(1, 2)
-selectTissue.SetInValue(255)
-selectTissue.SetOutValue(0)
-selectTissue.SetInputConnection(reader.GetOutputPort())
+def main():
+  fileName = 'seg.mhd'
+  fileName = os.path.join(filedir, fileName)
+  print(fileName)
+  reader = vtk.vtkMetaImageReader()
+  reader.SetFileName(fileName)
+  reader.Update()
 
-from timeit import default_timer as timer
+  selectTissue = vtk.vtkImageThreshold()
+  selectTissue.ThresholdBetween(1, 2)
+  selectTissue.SetInValue(255)
+  selectTissue.SetOutValue(0)
+  selectTissue.SetInputConnection(reader.GetOutputPort())
 
-start = timer()
-gaussianRadius = 1
-gaussianStandardDeviation = 2.0
-gaussian = vtk.vtkImageGaussianSmooth()
-gaussian.SetStandardDeviations(gaussianStandardDeviation, gaussianStandardDeviation, gaussianStandardDeviation)
-gaussian.SetRadiusFactors(gaussianRadius, gaussianRadius, gaussianRadius)
-gaussian.SetInputConnection(selectTissue.GetOutputPort())
+  from timeit import default_timer as timer
 
-#isoValue = 20.5#127.5 # Was 127.5
-isoValue = 127.5 # Was 127.5
-mcubes = vtk.vtkMarchingCubes()
-mcubes.SetInputConnection(gaussian.GetOutputPort())
-mcubes.ComputeScalarsOff()
-mcubes.ComputeGradientsOff()
-mcubes.ComputeNormalsOff()
-mcubes.SetValue(0, isoValue)
+  start = timer()
+  gaussianRadius = 1
+  gaussianStandardDeviation = 2.0
+  gaussian = vtk.vtkImageGaussianSmooth()
+  gaussian.SetStandardDeviations(gaussianStandardDeviation, gaussianStandardDeviation, gaussianStandardDeviation)
+  gaussian.SetRadiusFactors(gaussianRadius, gaussianRadius, gaussianRadius)
+  gaussian.SetInputConnection(selectTissue.GetOutputPort())
 
-smoothingIterations = 5 # was 3
-passBand = 0.001
-featureAngle = 60.0
-smoother = vtk.vtkWindowedSincPolyDataFilter()
-smoother.SetInputConnection(mcubes.GetOutputPort())
-smoother.SetNumberOfIterations(smoothingIterations)
-smoother.BoundarySmoothingOff() #smoother.BoundarySmoothingOn()
-smoother.FeatureEdgeSmoothingOff() # Turn off smoothing along sharp interior edges
-smoother.SetFeatureAngle(featureAngle) # Angle to distinguish a sharp edge
-smoother.SetPassBand(passBand)
-smoother.NonManifoldSmoothingOn()
-smoother.NormalizeCoordinatesOn()
-smoother.Update()
+  #isoValue = 20.5#127.5 # Was 127.5
+  isoValue = 127.5 # Was 127.5
+  mcubes = vtk.vtkMarchingCubes()
+  mcubes.SetInputConnection(gaussian.GetOutputPort())
+  mcubes.ComputeScalarsOff()
+  mcubes.ComputeGradientsOff()
+  mcubes.ComputeNormalsOff()
+  mcubes.SetValue(0, isoValue)
 
-end = timer()
-print('\nSmoothing: Seconds elapsed: %f' % (end - start))
+  smoothingIterations = 5 # was 3
+  passBand = 0.001
+  featureAngle = 60.0
+  smoother = vtk.vtkWindowedSincPolyDataFilter()
+  smoother.SetInputConnection(mcubes.GetOutputPort())
+  smoother.SetNumberOfIterations(smoothingIterations)
+  smoother.BoundarySmoothingOff() #smoother.BoundarySmoothingOn()
+  smoother.FeatureEdgeSmoothingOff() # Turn off smoothing along sharp interior edges
+  smoother.SetFeatureAngle(featureAngle) # Angle to distinguish a sharp edge
+  smoother.SetPassBand(passBand)
+  smoother.NonManifoldSmoothingOn()
+  smoother.NormalizeCoordinatesOn()
+  smoother.Update()
 
-normals = vtk.vtkPolyDataNormals()
-normals.SetInputConnection(smoother.GetOutputPort())
-normals.SetFeatureAngle(featureAngle)
+  end = timer()
+  print('\nSmoothing: Seconds elapsed: %f' % (end - start))
 
-stripper = vtk.vtkStripper()
-stripper.SetInputConnection(normals.GetOutputPort())
+  normals = vtk.vtkPolyDataNormals()
+  normals.SetInputConnection(smoother.GetOutputPort())
+  normals.SetFeatureAngle(featureAngle)
 
-# Connectivity filter
+  stripper = vtk.vtkStripper()
+  stripper.SetInputConnection(normals.GetOutputPort())
 
-connectFilter = vtk.vtkPolyDataConnectivityFilter()
-connectFilter.SetInputConnection(stripper.GetOutputPort())
-connectFilter.SetExtractionModeToSpecifiedRegions()
-connectFilter.AddSpecifiedRegion(0)
-connectFilter.Update();
+  # Connectivity filter
 
-#cleaner = vtk.vtkCleanPolyData()
-#cleaner.SetInputConnection(connectFilter.GetOutputPort())
-cleaner = connectFilter
-#cleaner = stripper
+  connectFilter = vtk.vtkPolyDataConnectivityFilter()
+  connectFilter.SetInputConnection(stripper.GetOutputPort())
+  #connectFilter.SetExtractionModeToSpecifiedRegions()
+  #connectFilter.AddSpecifiedRegion(0)
+  connectivityFilter->SetExtractionModeToLargestRegion()
+  connectFilter.Update();
 
-writer = vtk.vtkXMLPolyDataWriter()
-writer.SetFileName("/home/jmh/bkmedical/data/CT/A.vtp")
-writer.SetInputConnection(cleaner.GetOutputPort())
-writer.Write()
+  #cleaner = vtk.vtkCleanPolyData()
+  #cleaner.SetInputConnection(connectFilter.GetOutputPort())
+  cleaner = connectFilter # TODO: Make this work all the time
+  #cleaner = stripper
+
+  writer = vtk.vtkXMLPolyDataWriter()
+  writer.SetFileName("./A.vtp")
+  writer.SetInputConnection(cleaner.GetOutputPort())
+  writer.Write()
 
 # Convert from RAS to LPS
+if __name__ == '__main__':
+  main()
