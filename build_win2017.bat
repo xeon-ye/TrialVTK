7f99a03c5f0f258467a1c557a1f5b22b02a44df8
Jens Munk Hansen <jem@bkmedical.com>
2020-05-25 16:34:42 +0200

    ..

--- build_win2017.bat
+++ build_win2017.bat
@@ -20,11 +20,11 @@ cd %~dp0\build
 REM Build solution
 msbuild TrialVTK.sln /p:Configuration=Release
 
-cd %~dp0\build\Registration\Registration\Release
+cd %~dp0\build\Release
 
 set CURRENTDRIVE=%CD:~0,2%
 %QTDIR%\bin\qtenv2.bat
-cd /D %CURRENTDRIVE%%~dp0\build\Registration\Registration\Release
+cd /D %CURRENTDRIVE%%~dp0\build\Release
 %QTDIR%\bin\windeployqt.exe Registration.exe
 
 cd %~dp0
